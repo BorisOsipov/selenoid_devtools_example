dbaeb06e3048cb34c795814589a49c4e2c786f68
Boris Osipov <osipov.boris@gmail.com>
2019-01-12 19:48:18 +0300

    init

---
+++ test2.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const axios = require("axios");
+
+(async () => {
+    try {
+        const selenoidUrl = "10.20.12.138";
+        const {data} = await axios.post(`http://${selenoidUrl}:4444/wd/hub/session`,
+            {
+                "desiredCapabilities":
+                    {
+                        browserName: "chrome",
+                        "selenoid:options": {
+                            sessionTimeout: '3m',
+                            enableVnc: true
+                        }
+
+                    }
+            });
+        const sessionId = data.sessionId;
+
+        console.log(`DevTools url ws://10.20.12.138:4444/devtools/${sessionId}`);
+        const connect = await puppeteer.connect({browserWSEndpoint: `ws://10.20.12.138:4444/devtools/${sessionId}`});
+
+        const page = await connect.newPage();
+        await connect.close();
+    } catch (e) {
+        console.log("Error");
+        console.log(e);
+    }
+})();
